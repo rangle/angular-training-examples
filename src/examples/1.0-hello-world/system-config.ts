7b1603c634918e55270020bcab922ac6ac90d420
Michael Bennett <bennett000@users.noreply.github.com>
2016-09-27 12:58:26 -0400

    Feat pipes forms router (#14)
    * (WIP) Adds forms/pipes to all systemjs configs
    
    * (WIP) - Adds pipe examples
    
    * (WIP) - Adds examples for template forms
    
    * (WIP) - Adds form builder examples and form css example
    
    * Adds routing examples
    
    * (WIP) - Fixes glaring error in routing's <base href>'s

--- src/examples/1.0-hello-world/system-config.ts
+++ src/examples/1.0-hello-world/system-config.ts
@@ -19,9 +19,11 @@ declare var System: any;
     '@angular/core'                    : {main: 'bundles/core.umd.min.js'},
     '@angular/common'                  : {main: 'bundles/common.umd.min.js'},
     '@angular/compiler'                : {main: 'bundles/compiler.umd.min.js'},
+    '@angular/forms'                   : {main: 'bundles/forms.umd.min.js'},
+    '@angular/http'                    : {main: 'bundles/http.umd.min.js'},
     '@angular/platform-browser'        : {main: 'bundles/platform-browser.umd.min.js'},
     '@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'},
-    '@angular/http' : {main: 'bundles/http.umd.min.js'}
+    '@angular/router'                  : {main: 'bundles/router.umd.min.js'},
   };
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
--- src/examples/template/system-config.ts
+++ src/examples/template/system-config.ts
@@ -15,6 +15,15 @@ declare var System: any;
 
   /** User packages configuration. */
   const packages: any = {
+    'rxjs'                             : {main: 'Rx'},
+    '@angular/core'                    : {main: 'bundles/core.umd.min.js'},
+    '@angular/common'                  : {main: 'bundles/common.umd.min.js'},
+    '@angular/compiler'                : {main: 'bundles/compiler.umd.min.js'},
+    '@angular/forms'                   : {main: 'bundles/forms.umd.min.js'},
+    '@angular/http'                    : {main: 'bundles/http.umd.min.js'},
+    '@angular/platform-browser'        : {main: 'bundles/platform-browser.umd.min.js'},
+    '@angular/platform-browser-dynamic': {main: 'bundles/platform-browser-dynamic.umd.min.js'},
+    '@angular/router'                  : {main: 'bundles/router.umd.min.js'},
   };
 
   ////////////////////////////////////////////////////////////////////////////////////////////////
@@ -22,22 +31,8 @@ declare var System: any;
    * Everything underneath this line is managed by the CLI.
    **********************************************************************************************/
   const barrels: string[] = [
-    // Angular specific barrels.
-    '@angular/core',
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/forms',
-    '@angular/http',
-    '@angular/router',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-
-    // Thirdparty barrels.
-    'rxjs',
-
     // App specific barrels.
-    'app',
-    'app/shared',
+    '../../app',
     'examples',
     'code'
     /** @cli-barrel */

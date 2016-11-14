5d212c946515b44e667c4de2aecd4f626e4c9b97
Adam Winick <sinelanguage@gmail.com>
2016-11-14 14:41:53 -0500

    updated ES6 Readme

--- language-examples/remedial-js/9.0-ES5-vs-ES6/app/exercise.js
+++ language-examples/remedial-js/9.0-ES5-vs-ES6/app/exercise.js
@@ -3,6 +3,6 @@
 
 const exampleDiv = document.getElementById('example');
 
-exampleDiv.innerHTML = 'Browser Rendering: See ReadMe file for definition, examples and exercises.  See browser console for code execution logs';
+exampleDiv.innerHTML = 'ES5 vs ES6: See ReadMe file for definition and examples of all ES6 features vs ES5.';
 
 

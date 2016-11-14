526de86b3bbf77100244d7580360a6a9ff50e443
Adam Winick <sinelanguage@gmail.com>
2016-11-14 14:36:34 -0500

    updated ES6 Readme

---
+++ language-examples/remedial-js/9.0-ES5-vs-ES6/app/exercise.js
@@ -0,0 +1,8 @@
+'use strict';
+'format es6'; // force SystemJS to transpile exercise
+
+const exampleDiv = document.getElementById('example');
+
+exampleDiv.innerHTML = 'Browser Rendering: See ReadMe file for definition, examples and exercises.  See browser console for code execution logs';
+
+

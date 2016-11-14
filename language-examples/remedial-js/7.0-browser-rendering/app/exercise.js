952a6d721b120e18cf9dd8e0e37b7f1c3c386d10
Adam Winick <sinelanguage@gmail.com>
2016-11-14 11:38:20 -0500

    file creation or exercise 7

---
+++ language-examples/remedial-js/7.0-browser-rendering/app/exercise.js
@@ -0,0 +1,6 @@
+'use strict';
+'format es6'; // force SystemJS to transpile exercise
+
+const exampleDiv = document.getElementById('example');
+
+exampleDiv.innerHTML = 'Source Maps: See ReadMe file for definition, examples and exercises';

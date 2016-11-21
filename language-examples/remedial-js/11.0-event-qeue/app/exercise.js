1bb8e471d5766fef445c458113784447fcc4eab0
Adam Winick <sinelanguage@gmail.com>
2016-11-21 14:29:41 -0500

    WIP on Event Qeue examples

--- language-examples/remedial-js/11.0-event-qeue/app/exercise.js
+++ language-examples/remedial-js/11.0-event-qeue/app/exercise.js
@@ -1,30 +1,4 @@
 'use strict';
 'format es6'; // force SystemJS to transpile exercise
 
-const exampleDiv = document.getElementById('example');
-
-exampleDiv.innerHTML = '';
-
-let httpRequest;
-
-function makeRequest(url) {
-  httpRequest = new XMLHttpRequest();
-
-  if (!httpRequest) {
-    alert('Giving up :( Cannot create an XMLHTTP instance');
-    return false;
-  }
-  httpRequest.onreadystatechange = alertContents;
-  httpRequest.open('GET', url);
-  httpRequest.send();
-};
-
-function alertContents() {
-  if (httpRequest.readyState === XMLHttpRequest.DONE) {
-    if (httpRequest.status === 200) {
-      alert(httpRequest.responseText);
-    } else {
-      alert('There was a problem with the request.');
-    }
-  }
-};
+const exampleDiv = document.getElementById('Please see Readme file for full details about the Event Qeue');
